36440ae067c92dfc5a112e0e08c3cc38f0514b06
Eryhnar <pedrofernbel@gmail.com>
2024-03-25 11:55:14 +0100

    feat: created CButton element.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import "./Login.css";
 import { CInput } from "../../common/CInput/CInput";
 import { Header } from "../../common/Header/Header";
+import { CButton } from "../../common/CButton/CButton";
 
 export const Login = () => {
   return (
@@ -8,13 +9,27 @@ export const Login = () => {
       <Header />
       <div className="loginDesign">
         <CInput
-          className="input"
-          type="text"
-          placeholder="John"
-          name="name"
-          disabled={false}
+          className="inputDesign"
+          type="email"
+          placeholder="email@yourdomain.com"
+          name="email"
+          disabled=""
           value=""
           onChangeFunction={() => { }}
+        />
+        <CInput
+          className="inputDesign"
+          type="password"
+          placeholder="password"
+          name="password"
+          disabled=""
+          value=""
+          onChangeFunction={() => { }}
+        />
+        <CButton
+          className="loginButton"
+          title="Log in"
+          onClickFunction={() => { }}
         />
       </div>
     </>
