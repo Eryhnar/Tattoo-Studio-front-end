6ae9f545f72958dbbd10be97a13b800dc8457d68
pfernandez <pedrofernbel@gmail.com>
2024-03-27 00:23:11 +0100

    style: added some style to the Profile view.

--- src/common/CCard/CCard.jsx
+++ src/common/CCard/CCard.jsx
@@ -1,11 +1,11 @@
-import "./customCard.css";
+import "./CCard.css";
 
-export const CustomCard = ({ className, title, content, image }) => {
+export const CCard = ({ className, title, content, image }) => {
     return (
         <div className={className}>
             <h1>{title}</h1>
             {image && <img src={image} alt="image not found" />}
-            <p>{content}</p>
+            <div>{content}</div>
         </div>
     )
 }
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import { GetProfileService } from "../../services/apiCalls";
 import "./Profile.css";
 import { TokenContext } from "../../App";
+import { CCard } from "../../common/CCard/CCard";
+import { CInput } from "../../common/CInput/CInput";
+import { CButton } from "../../common/CButton/CButton";
 
 export const Profile = () => {
     const {token, setToken} = React.useContext(TokenContext);
@@ -39,8 +42,59 @@ export const Profile = () => {
     // console.log(profile);
 
     return (
-        <div className="profileDesign">
-            This is Profile
+        <div className="profile-design">
+            <CCard 
+                className={"profile-card"}
+                title={profile.name} 
+                content={
+                    <div className="profile-inputs">
+                        <div className="name-field">
+                            <p>Name</p>
+                            <CInput 
+                                className={"profile-input"}
+                                type="name"
+                                name="name"
+                                disabled="disabled"
+                                value={profile.name || ""}
+                                onChangeFunction={() => {}}
+                            />
+                            <div className="edit-icon"></div>
+                        </div>
+                        <div className="surname-field">
+                            <p>Surname</p>
+                            <CInput 
+                                className={"profile-input"}
+                                type="surname"
+                                name="surname"
+                                disabled="disabled"
+                                value={profile.surname || ""}
+                                onChangeFunction={() => {}}
+                            />
+                        </div>
+                        <div className="email-field">
+                            <p>Email</p>
+                            <CInput 
+                                className={"profile-input"}
+                                type="email"
+                                name="email"
+                                disabled="disabled"
+                                value={profile.email || ""}
+                                onChangeFunction={() => {}}
+                            />
+                        </div>
+                        <CButton
+                            className={"change-password-button"}
+                            title={"change password"}
+                            onClickFunction={() => {}}
+                        />
+                        <CButton
+                            className={"edit-profile-button"}
+                            title={"edit profile"}
+                            onClickFunction={() => {}}
+                        />
+                    </div>
+                }
+            />
         </div>
     );
 }
