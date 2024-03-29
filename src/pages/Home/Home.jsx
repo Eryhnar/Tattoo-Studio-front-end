933fd98afc77c71eb91f5e190281a7e3a2c093c5
pfernandez <pedrofernbel@gmail.com>
2024-03-29 23:15:49 +0100

    feat: added content to Home.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -29,8 +29,8 @@ export const Home = () => {
     
     return (
         // <div className="viewDesign">
-        <>
-            <div className="homeDesign">
+        <div className="homeDesign">
+            {/* <div className="homeDesign">
                 <CDropdown 
                     buttonClass="dropdown"
                     dropdownClass="dropdown-content"
@@ -45,8 +45,32 @@ export const Home = () => {
                     items={services}
                     onChangeFunction={()=>{}}
                 />
-            </div>
-        </>
+            </div> */}
+            <h1>Inkdin</h1>
+            <section>
+                <div>
+                    <img src="https://images.unsplash.com/photo-1612838320302-4b3b3b3b3b3b" alt="tattoo" />
+                    <h2>Show your personality</h2>
+                    <p>We offer the best selection of piercings and the best artists to let you express yourself freely</p>
+                </div>
+                <article>
+                    <h2>The best tattoo artists in San Francisco</h2>
+                    <p>
+                        Our artists are the best in the world. We have artists specialized in many 
+                        different styles from tribal, fine line, watercolor to UV tattoos
+                    </p>
+                </article>
+                <article>
+                    <h2>Right in the heart of San Francisco</h2>
+                    <p>
+                        Our studio is located in the heart of San Francisco. 
+                        It is designed to make you feel comfortable and relaxed during your time with us.
+                    </p>
+                </article>
+                <p>We are open from Monday to Saturday from 10:00 to 20:00</p>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d8919.84982051217!2d-122.42486454269346!3d37.77496479366142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1stattoo!5e0!3m2!1ses!2ses!4v1711750331416!5m2!1ses!2ses" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </section>
+        </div>
         // </div>
     );
 }
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,86 +1,117 @@
 const root = "http://localhost:4000/api/";
 
 export const RegisterService = async (user) => {
-    const response = await fetch(root + "register", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-    });
+    try {
+        const response = await fetch(root + "register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        });
 
-    return response.json();
+        return response.json();
+    } catch (error) {
+        throw error;
+    }
 }
 
 export const LoginService = async (user) => {
-    const response = await fetch(root + "login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-    });
+    try {
+        const response = await fetch(root + "login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        });
 
-    return response.json();
+        return response.json();
+    } catch (error) {
+        throw error;
+    }
 }
 
 export const GetProfileService = async (token) => {
-    const response = await fetch(root + "users/profile", {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return response.json();
+    try {
+        const response = await fetch(root + "users/profile", {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return response.json();
+    } catch (error) {
+        throw error;
+    }
 }
 
 export const UpdateProfileService = async (user, token) => {
-    const response = await fetch(root + "users/profile", {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(user),
-    });
+    try {
+        const response = await fetch(root + "users/profile", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(user),
+        });
 
-    return response.json();
+        return response.json();
+    } catch (error) {
+        throw error;
+    }
 }
 
 export const GetServicesService = async () => {
-    const response = await fetch(root + "services", {
-        method: "GET",
-    });
-    return response.json();
+    try {
+        const response = await fetch(root + "services", {
+            method: "GET",
+        });
+        return response.json();
+    } catch (error) {
+        throw error;
+    }
 }
 
 export const GetAppointmentsService = async (state, token) => {
-    // const response = await fetch(root + "appointments/user" + , {
-    const response = await fetch(`${root}appointments/user?status=${state}`, {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    return response.json();
+    try {
+        const response = await fetch(`${root}appointments/user?status=${state}`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return response.json();
+    } catch (error) {
+        throw error;
+    }
 }
 
 export const CreateAppointmentService = async (appointment, token) => {
-    const response = await fetch(root + "appointments", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(appointment),
-    });
+    try {
+        const response = await fetch(root + "appointments", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(appointment),
+        });
 
-    return response.json();
+        return response.json();
+    } catch (error) {
+        throw error;
+    }
 }
 
 export const GetArtistsService = async () => {
-    const response = await fetch(root + "users/artists", {
-        method: "GET",
-    });
-    return response.json();
+    try {
+        const response = await fetch(root + "users/artists", {
+            method: "GET",
+        });
+        return response.json();
+    } catch (error) {
+        throw error;
+    }
 }
