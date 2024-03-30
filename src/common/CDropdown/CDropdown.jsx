3db79c07f728376c01a0aca514a5d796d6608d42
pfernandez <pedrofernbel@gmail.com>
2024-03-30 20:14:39 +0100

    style: Home view formatted.

--- src/common/CDropdown/CDropdown.jsx
+++ src/common/CDropdown/CDropdown.jsx
@@ -1,47 +1,5 @@
 import "./CDropdown.css";
 
-// export const CDropdown = ({ title, items }) => {
-//     // return (
-//     //     <div className="dropdown">
-//     //         <button className="dropbtn">{title}</button>
-//     //         <div className="dropdown-content">
-//     //             {items.map((item, index) => (
-//     //                 <a key={index} href={item.path}>{item.title}</a>
-//     //             ))}
-//     //         </div>
-//     //     </div>
-//     // );
-
-//     return (
-//         <select>
-//           {items.map(item => (
-//             <option key={item.id} value={item.id}>
-//               {item.name}
-//             </option>
-//           ))}
-//         </select>
-//       );
-// }
-
-// export const CDropdown = ({ buttonClass, dropdownClass, title, items }) => {
-//     const [isOpen, setIsOpen] = useState(false);
-
-//     const toggleOpen = () => setIsOpen(!isOpen);
-
-//     return (
-//         <div className={buttonClass}>
-//             <button className="dropbtn" onClick={toggleOpen}>{title}</button>
-//             {isOpen && (
-//                 <div className={dropdownClass}>
-//                     {items.map((item, index) => (
-//                         <div className="dropdown-item" key={index}>{item}</div> 
-//                     ))}
-//                 </div>
-//             )}
-//         </div>
-//     );
-// }
-
 export const CDropdown = ({ buttonClass, dropdownClass, title, items, onChangeFunction }) => {
     return (
         <select className={buttonClass} onChange={onChangeFunction} name={title} defaultValue={""}>
@@ -52,13 +10,3 @@ export const CDropdown = ({ buttonClass, dropdownClass, title, items, onChangeFu
         </select>
     );
 }
-// export const CDropdown = ({ buttonClass, dropdownClass, title, items, onChangeFunction }) => {
-//     return (
-//         <select className={buttonClass} onChange={onChangeFunction} name={title}>
-//             <option value="" disabled selected> {title} </option>
-//             {items.map((item, index) => (
-//                 <option key={index} value={item} className={dropdownClass}>{item}</option>
-//             ))}
-//         </select>
-//     );
-// }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -62,29 +62,42 @@ export const Home = () => {
                 </div> */}
                 <article>
                     <div className="home-article-left">
-                        <div className="home-article-text">
-                            <h2>The best tattoo artists in San Francisco</h2>
-                            <p>
-                                Our artists are the best in the world. We have artists specialized in many
-                                different styles from tribal, fine line, watercolor to UV tattoos. Check out our catalogue!
-                            </p>
-                        </div>
-                        <div className="home-article-image-container">
-                            <img src="https://i.imgur.com/sWMP0Q7.jpeg" alt="tattoo" />
+                        <div className="home-article-grid">
+                            <div className="home-article-text">
+                                <h2>The best tattoo artists in San Francisco</h2>
+                                <p>
+                                    We have artists specialized in many
+                                    different styles from tribal, fine line, watercolor to UV tattoos. Check out our catalogue!
+                                </p>
+                            </div>
+                            <div className="home-article-image-container">
+                                <img src="https://i.imgur.com/sWMP0Q7.jpeg" alt="tattoo" />
+                            </div>
                         </div>
                     </div>
                 </article>
                 <article>
-                    <h2>Right in the heart of San Francisco</h2>
-                    <p>
-                        Our studio is located in the heart of San Francisco. 
-                        It is designed to make you feel comfortable and relaxed during your time with us.
-                    </p>
-                    <p>We are open from Monday to Saturday from 10:00 to 20:00</p>
-                    <div>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d8919.84982051217!2d-122.42486454269346!3d37.77496479366142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1stattoo!5e0!3m2!1ses!2ses!4v1711750331416!5m2!1ses!2ses" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <div className="home-article-right">
+                        <div className="home-article-grid">
+                            <div className="home-article-image-container">
+                                <img src="https://i.imgur.com/lnmPhOA.jpeg" alt="tattoo" />
+                            </div>
+                            <div className="home-article-text">
+                                <h2>Right in the heart of San Francisco</h2>
+                                <p>
+                                    Our studio is located in the heart of San Francisco.
+                                    It is designed to make you feel comfortable and relaxed during your time with us.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </article>
+                    <div className="home-where">
+                        <h2>Where to find us</h2>
+                        <p>We are open from Monday to Saturday from 10:00 to 20:00</p>
+                        <p>Contact us: <a href="https://www.linkedin.com/in/pedro-fernandez-bel-68a2b9155/">here</a></p>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d8919.84982051217!2d-122.42486454269346!3d37.77496479366142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1stattoo!5e0!3m2!1ses!2ses!4v1711750331416!5m2!1ses!2ses" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
             </section>
         </div>
         // </div>
