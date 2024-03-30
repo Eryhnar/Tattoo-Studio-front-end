aeb31c8df8b450db4c1a5e773955c31350385e7c
pfernandez <pedrofernbel@gmail.com>
2024-03-30 23:26:42 +0100

    style: added style to Home view.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -96,7 +96,9 @@ export const Home = () => {
                         <h2>Where to find us</h2>
                         <p>We are open from Monday to Saturday from 10:00 to 20:00</p>
                         <p>Contact us: <a href="https://www.linkedin.com/in/pedro-fernandez-bel-68a2b9155/">here</a></p>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d8919.84982051217!2d-122.42486454269346!3d37.77496479366142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1stattoo!5e0!3m2!1ses!2ses!4v1711750331416!5m2!1ses!2ses" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <div className="home-map-container">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d8919.84982051217!2d-122.42486454269346!3d37.77496479366142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1stattoo!5e0!3m2!1ses!2ses!4v1711750331416!5m2!1ses!2ses" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
                     </div>
             </section>
         </div>
