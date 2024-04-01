1947423f9b1f4095f90f9a267b8946d7077f8e4c
pfernandez <pedrofernbel@gmail.com>
2024-04-01 11:07:14 +0200

    feat: added burger menu to header.

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.css";
 import { NavButton } from "../NavButton/NavButton";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TokenContext } from "../../App";
 import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
@@ -10,11 +10,36 @@ export const Header = () => {
     const navigate = useNavigate();
     const { token, setToken } = React.useContext(TokenContext);
 
+    const [menuOpen, setMenuOpen] = useState(false);
+
     const logOut = () => {
         setToken(null);
         localStorage.removeItem("token");
         navigate("/");
     };
+
+    // useEffect(() => {
+    //     if (!menuOpen) {
+    //         setTimeout(() => {
+    //             document.querySelector(".burger-menu").style.display = "none";
+    //         }, 500);
+    //     } else {
+    //         document.querySelector(".burger-menu").style.display = "block";
+    //     }
+    // }, [menuOpen]);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+        // if (menuOpen) {
+        //     document.querySelector(".burger-menu").style.display = "block";
+        // }
+
+        // if (!menuOpen) {
+        //     setTimeout(() => {
+        //         document.querySelector(".burger-menu").style.display = "none";
+        //     }, 500);
+        // }
+    }
 
     return (
         <div className="header-design">
@@ -26,19 +51,98 @@ export const Header = () => {
                     Home
                 </NavLink>
                 {/* <NavButton title="Home" path="/" /> */}
-                <NavLink
-                    to="/services"
-                    className={"nav-button-design"}
-                >
-                    Services
-                </NavLink>
-                <NavLink
-                    to="/catalogue"
-                    className={"nav-button-design"}
-                >
-                    Catalogue
-                </NavLink>
+                <div className="hidden-nav">
+                    <NavLink
+                        to="/services"
+                        className={"nav-button-design"}
+                    >
+                        Services
+                    </NavLink>
+                    <NavLink
+                        to="/catalogue"
+                        className={"nav-button-design"}
+                    >
+                        Catalogue
+                    </NavLink>
+                </div>
                 {/* <NavButton title="Services" path="/services" /> */}
+            </div>
+            <div className="burger-button" onClick={toggleMenu}>{menuOpen 
+                ? <span class="material-symbols-outlined">menu_open</span> 
+                : <span class="material-symbols-outlined">
+                menu
+                </span>}</div>
+            <div className={`burger-menu ${menuOpen ? "" : "burger-menu-hidden"}`}>
+                <ul>
+                    <li>
+                        <NavLink
+                            to="/services"
+                            className={"nav-button-design"}
+                        >
+                            Services
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/catalogue"
+                            className={"nav-button-design"}
+                        >
+                            Catalogue
+                        </NavLink>
+                    </li>
+                    {token ? (
+                        <>
+                            {token.roleName.includes("admin") ? (
+                                <li>
+                                    <NavLink
+                                        to="/admin"
+                                        className={"nav-button-design"}
+                                    >
+                                        Admin
+                                    </NavLink>
+                                </li>
+                            ) : null}
+                            <li>
+                                <NavLink
+                                    to="/appointments"
+                                    className={"nav-button-design"}
+                                >
+                                    Appointments
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/profile"
+                                    className={"nav-button-design"}
+                                >
+                                    {token.name}
+                                </NavLink>
+                            </li>
+                            <li onClick={logOut}>
+                                <NavButton title="Logout" path="/" />
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            <li>
+                                <NavLink
+                                    to="/register"
+                                    className={"nav-button-design"}
+                                >
+                                    Register
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/login"
+                                    className={"nav-button-design"}
+                                >
+                                    Login
+                                </NavLink>
+                            </li>
+                        </>
+                    )}
+                </ul>
             </div>
             <div className="header-right">
                 {token ? (
@@ -57,22 +161,13 @@ export const Header = () => {
                         >
                             Appointments
                         </NavLink>
-                        {/* <NavButton title="Appointments" path="/appointments"/> */}
-                        <NavLink 
+                        <NavLink
                             to="/profile"
                             className={"nav-button-design"}
-                            // activeClassName="active-nav-button"
                         >
                             {token.name}
                         </NavLink>
-                        {/* <NavButton title={token.name} path="/profile" /> */}
                         <div onClick={logOut}>
-                            {/* <NavLink
-                                to="/"
-                                className={"nav-button-design"}
-                            >
-                                Logout
-                            </NavLink> */}
                             <NavButton title="Logout" path="/" />
                         </div>
                     </>
@@ -84,14 +179,12 @@ export const Header = () => {
                         >
                             Register
                         </NavLink>
-                        {/* <NavButton title="Register" path="/register" /> */}
                         <NavLink
                             to="/login"
                             className={"nav-button-design"}
                         >
                             Login
                         </NavLink>
-                        {/* <NavButton title="Login" path="/login" /> */}
                     </>
                 )}
             </div>
