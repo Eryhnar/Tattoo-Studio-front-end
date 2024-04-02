8e5170a9c667e0fbaa97b9fcbe6d83589dd81b8a
pfernandez <pedrofernbel@gmail.com>
2024-04-02 19:29:56 +0200

    change: swapped Home banner text colors for readability.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,32 +1,6 @@
-import { CDropdown } from "../../common/CDropdown/CDropdown";
-import { GetServicesService } from "../../services/apiCalls";
 import "./Home.css";
-import { useEffect, useState } from "react";
 
 export const Home = () => {
-    const [services, setServices] = useState([]);
-    const [artists, setArtists] = useState([]);
-    const [catalogues, setCatalogues] = useState([]);
-
-    // useEffect(() => {
-    //     const fetchServices = async () => {
-    //         const response = await GetServicesService();
-    //         const data = response.data;
-    //         setServices(data);
-
-    //         setTimeout(() => {
-    //             console.log("response",response);
-    //             console.log("data",data);
-    //             console.log("services",services);
-    //         }, 2000);
-    //     };
-    //     if (services.length === 0) {
-    //         fetchServices();
-    //     }
-
-        
-    // }, []);
-    
     return (
         // <div className="viewDesign">
         <div className="homeDesign">
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -2,6 +2,7 @@ import "./Services.css";
 import { useState, useEffect } from "react";
 import { GetServicesService } from "../../services/apiCalls";
 import { CCard } from "../../common/CCard/CCard";
+import { LoadingScreen } from "../../common/LoadingScreen/LoadingScreen";
 
 export const Services = () => {
     const [services, setServices] = useState([]);
@@ -19,25 +20,32 @@ export const Services = () => {
         if (services.length === 0) {
             fetchServices();
         }
-    }, []);
+    }, [services]);
 
     return (
-        <div className="services-design">
-                <h1>Services</h1>
-            <div className="services-banner"></div>
-            {/* <p>Check out our services!</p> */}
-            <div className="services-body">
-                <div className="services">
-                    {services.map((service, index) => (
-                        <CCard
-                            className= {"service"}
-                            title= {service.name}
-                            content= {service.description}
-                            image= {service.photo}
-                        />
-                    ))}
-                </div>
-            </div>
-        </div>
+        <>
+            {services.length === 0 
+                ? 
+                    <LoadingScreen />
+                : 
+                    <div className="services-design">
+                            <h1>Services</h1>
+                        <div className="services-banner"></div>
+                        {/* <p>Check out our services!</p> */}
+                        <div className="services-body">
+                            <div className="services">
+                                {services.map((service, index) => (
+                                    <CCard
+                                        className= {"service"}
+                                        title= {service.name}
+                                        content= {service.description}
+                                        image= {service.photo}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+            }
+        </>
     );
 }
